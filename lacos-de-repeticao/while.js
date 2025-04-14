7de13f66fbe6f5a6f33d07379bdb037782a484a6
Jovic_Penhaz <victosousa92@gmail.com>
2025-04-14 19:44:35 -0300

    while

---
+++ lacos-de-repeticao/while.js
@@ -0,0 +1,14 @@
+// let i = 0
+
+//     while (i <= 10) {
+
+//         alert(`A variavel i é ${i}`)
+//         i++
+//     }
+
+let i = 1;
+    do {
+        alert(`A variavel i é ${i}`)
+        i++
+    } while (i <= 10)
+        
