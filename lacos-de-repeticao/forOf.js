224bebeba84abe2fea261be613800e9cefcbb69a
Jovic_Penhaz <victosousa92@gmail.com>
2025-04-14 20:38:03 -0300

    for of

---
+++ lacos-de-repeticao/forOf.js
@@ -0,0 +1,14 @@
+// const frutas = ["laranja", "maracujá", "limão"];
+
+// for (let fruta of frutas) {
+//     alert(fruta)
+// }
+
+const notas = [10, 5, 6, 7, 8]
+
+let somaTotal = 0
+
+for (let nota of notas) {
+    // somaTotal = somaTotal + nota
+    somaTotal += nota
+}
